90ba2bac059d9ca60b86c7a69a9987d53153229e
Omer Dotchamou <ddaniomer@gmail.com>
2023-01-18 20:28:57 +0100

    Tailwind setup + Header component set up

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import CV from '../assets/cv.pdf'
+
+const Header =  () =>{
+    return(
+        <div className='fixed top-0 mt-0 z-50 w-full'>
+            {/***** Navigation *****/}
+            <nav className='pt-10 pb-5 px-2 sm:px-8 mb-10 flex justify-between items-center bg-green-50 shadow-md dark:bg-slate-900'>
+                <h1 className='text-xl font-burtons dark:text-gray-300'>developedbyOD</h1>
+                <ul className='flex items-center'>
+                    <li>
+                        <a className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 hover:bg-white hover:text-black py-2 border-none rounded-md ml-4 sm:ml-8"
+                        href={CV}
+                        download
+                        >
+                        Resume
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
